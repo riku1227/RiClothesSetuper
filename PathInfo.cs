e87c990e592be71f6840c8af6145308d418b7352
riku1227 <17927874+riku1227@users.noreply.github.com>
2020-04-25 13:27:27 +0900

    :tada: Happy initial commit!

---
+++ PathInfo.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PathInfo : MonoBehaviour
+{
+    public string basePath;
+    public string jsonPath;
+    public string languagePath;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # RiClothesSetuper
 アバター専用服製作者向け着替えUnity Editor拡張
+
+## ドキュメント
+まだ書けてない...
