ec43e3ff388a8b1fdff6bd4a79d2d998fc99f1fb
riku1227 <17927874+riku1227@users.noreply.github.com>
2021-07-26 22:37:41 +0900

    :bookmark: v2.1.0

--- Editor/RiClothesSetuper.cs
+++ Editor/RiClothesSetuper.cs
@@ -4,19 +4,20 @@ using UnityEditor;
 
 namespace RiClothes {
     public class RiClothesSetuper: MonoBehaviour {
+        static string SETUPER_VERSION = "v2.1.0";
         [MenuItem("RiClothes/Setuper")]
         static void Setup() {
-            EditorWindow.GetWindow<Setuper> (true, "RiClothes Setuper");
+            EditorWindow.GetWindow<Setuper> (true, "RiClothes Setuper " + SETUPER_VERSION);
         }
 
         [MenuItem("RiClothes/DiffTextureGenerator")]
         static void DiffTextureGenerator() {
-            EditorWindow.GetWindow<DiffTexGenerator> (true, "Diff Texture Generator");
+            EditorWindow.GetWindow<DiffTexGenerator> (true, "Diff Texture Generator " + SETUPER_VERSION);
         }
 
         [MenuItem("RiClothes/Cloth UnInstaller")]
         static void ClothUnInstaller() {
-            EditorWindow.GetWindow<ClothUnInstallerWindow> (true, "RiClothes Cloth Uninstaller");
+            EditorWindow.GetWindow<ClothUnInstallerWindow> (true, "RiClothes Cloth Uninstaller " + SETUPER_VERSION);
         }
     }
 }
